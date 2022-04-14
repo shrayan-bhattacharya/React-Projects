60bf692b5317aab1f84ec6d57da36a004f4fb5a7
Shrayan Bhattacharya <shrayanjob08@gmail.com>
2022-04-14 13:13:45 +0530

    Added Few Events

--- Expense Tracker App/src/App.js
+++ Expense Tracker App/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
   const expenses = [
@@ -26,6 +26,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -35,7 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
