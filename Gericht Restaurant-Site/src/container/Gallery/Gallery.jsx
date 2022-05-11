d0de8aa0fed4c3a900c844fafa0691d72d15ce08
Shrayan Bhattacharya <shrayanjob08@gmail.com>
2022-05-11 12:05:58 +0530

    Completed the Awards Section

--- Gericht Restaurant-Site/src/container/Gallery/Gallery.jsx
+++ Gericht Restaurant-Site/src/container/Gallery/Gallery.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from 'react'
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from 'react-icons/bs'
 
-import './Gallery.css';
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
+import './Gallery.css'
+const Gallery = () => {
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p
+          className="p__opensans"
+          style={{ color: '#AAAAAA', marginTop: '2rem' }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+    </div>
+  )
+}
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
-
-export default Gallery;
+export default Gallery
